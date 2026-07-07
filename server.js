d21a0658f9ecedcc04236f5ad0fa1c7069c7bc7a
Riya Rikame <your-email@gmail.com>
2026-07-07 22:58:21 +0530

    Fixed Render deployment

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-// ✅ FIXED ROUTE NAME
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
@@ -19,14 +20,22 @@ app.use("/api/reviews", require("./routes/reviewRoutes"));
 app.use("/api/wishlist", require("./routes/wishlistRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/payment", require("./routes/paymentRoutes"));
+
 app.get("/", (req, res) => {
- res.sendFile(__dirname + "/public/register.html");
+    res.sendFile(path.join(__dirname, "public", "register.html"));
 });
 
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected ✅");
+})
+.catch((err) => {
+    console.log("Mongo Error ❌", err);
+});
 
-// MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected ✅"))
-  .catch(err => console.log("Mongo Error ❌", err));
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log("Server running on port 5000 🚀"));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 🚀`);
+});
